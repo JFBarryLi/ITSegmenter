115c5faa69d5f30b9bfc4ba4cdf163423cfd25ec
JFBarryLi <40674314+JFBarryLi@users.noreply.github.com>
2018-07-09 11:22:41 -0400

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -1,26 +1,37 @@
 function submit() {
-/* 	
+/* 
  * Parameters:
  * -----------
+ * imgPath: 	String
+ * 				File path to the image
+ *
  * fThreshhold: int
- *		Fast Threshhold; Default:100, higher = less corners
+ * 				Fast Threshhold; Default:100, higher = less corners
  *
- * eps: int
- *      Maximum distance between two points to be considered neighbours
+ * eps: 		int
+ *      		Maximum distance between two points to be considered neighbours; Default:15
  *
- * minPts: int
- *		   Minimum number of points required to form a cluster
+ * minPts: 		int
+ *		   		Minimum number of points required to form a cluster; Default:5
  *
- * sharpness: float
- * 			  sharpness filter parameter; Default:0.6 
+ * sharpness: 	float
+ * 				Sharpness filter parameter; Default:0.6
  *
- * imgPath: String
- * 			File path to the image
+ * drawRects: 	bol
+ * 				Option to draw bounding boxes on the image; Default:0
  *
- */	
+ * splitRects: 	bol
+ * 				Option to split the text segments into individual images; Default:0
+ *
+ * Returns:
+ * --------
+ * rects:		obj
+ *				Dicitonary of clusters and their corresponding bounding box. rects {} = {key = 1 : value = [[xMin1, xMax1, yMin1, yMax1],...],...}
+ * 
+ */
 	imgPath = document.getElementById('pathInput').value;
 	
-	textSegment(100, 15, 5, 0.6, imgPath);
+	textSegment(imgPath, 100, 15, 5, 0.6, 1, 1);
 
 }
 
