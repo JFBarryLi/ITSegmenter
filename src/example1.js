c3e7360f051b924bfb6b1f936380f468b9c308a7
JFBarryLi <40674314+JFBarryLi@users.noreply.github.com>
2018-07-25 14:51:59 -0400

    Update example1.js

--- src/example1.js
+++ src/example1.js
@@ -2,7 +2,7 @@ function submit() {
 
 	imgPath = document.getElementById('pathInput').value;
 	
-	textSegment(imgPath, 100, 15, 5, 10, 1, 1, 1);
+	textSegment(imgPath, 60, 15, 5, 10, 1, 1, 1);
 
 }
 
