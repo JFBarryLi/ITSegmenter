3705a79c9d97ba30ea8ed987b40d724d6f1655b1
JFBarryLi <40674314+JFBarryLi@users.noreply.github.com>
2018-07-25 14:55:47 -0400

    Update autocrop.js

--- src/autocrop.js
+++ src/autocrop.js
@@ -28,8 +28,8 @@ function submit() {
 	if (thresh == "") {thresh = 60;}
 	if (eps == "") {eps = 15;}
 	if (minPts == "") {minPts = 5;}
-	if (dia == "") {sharp = 10;}
-	if (amt == "") {sharp = 1;}
+	if (dia == "") {dia = 10;}
+	if (amt == "") {amt = 1;}
 	
 	// var f1 = function () {
 		// var r = $.Deferred();
