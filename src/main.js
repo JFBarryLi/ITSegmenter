37692ac3035d65fc92081f81a5a8566ae54c51b0
JFBarryLi <40674314+JFBarryLi@users.noreply.github.com>
2018-08-01 15:27:55 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,3 +6,27 @@ $(function () {
 
     Path.listen();
 });
+
+$(function() {
+		var layout = '#layout';
+		var sbar = '#sbar';
+		var sidebarTog = '#sidebarTog';
+
+	function toggleClass(id) {
+		var classId = String($(id).attr('class'));
+		if(classId.indexOf('active') == -1) {
+			$(id).addClass('active');
+		} else {
+			$(id).removeClass('active');
+		}
+	}
+
+	$(sidebarTog)[0].addEventListener('click', function(e) {	  
+		toggleClass(layout);
+		toggleClass(sbar);
+		toggleClass(sidebarTog);
+		e.preventDefault();
+  });
+}());
+
+
