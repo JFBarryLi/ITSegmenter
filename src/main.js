7e4cb2f69dfab29c57be4827fd1a16e89def50da
JFBarryLi <40674314+JFBarryLi@users.noreply.github.com>
2018-08-30 08:50:30 -0400

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 $(function () {
 	Path.map("#/home").to(function(){$(window).stop(true,true).scrollTo('#main-title', 300)});
 	Path.map("#/intro").to(function(){$(window).stop(true,true).scrollTo('#intro', 300)});
-	Path.map("#/its").to(function(){$(window).stop(true,true).scrollTo('#its', 300)});
-	Path.map("#/batch").to(function(){$(window).stop(true,true).scrollTo('#batch', 300)});
+	Path.map("#/usm").to(function(){$(window).stop(true,true).scrollTo('#usm', 300)});
+	Path.map("#/fast").to(function(){$(window).stop(true,true).scrollTo('#fast', 300)});
+	Path.map("#/kdTree").to(function(){$(window).stop(true,true).scrollTo('#kdTree', 300)});
+	Path.map("#/dbscan").to(function(){$(window).stop(true,true).scrollTo('#dbscan', 300)});
+	Path.map("#/segImg").to(function(){$(window).stop(true,true).scrollTo('#segImg', 300)});
+
 
     Path.listen();
 });
